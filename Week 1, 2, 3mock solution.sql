c4c3f0066610a203f3f35fc5317e7e4cd6608dd2
Pooja Madhup <90482825+pooja9050@users.noreply.github.com>
2024-05-16 07:06:19 -0700

    Update Week 1, 2, 3mock solution.sql

--- Week 1, 2, 3mock solution.sql
+++ Week 1, 2, 3mock solution.sql
@@ -97,3 +97,11 @@ FROM Sales
 GROUP BY sale_date
 ORDER BY sale_date;
 
+
+#1407. Top Travellers using COALESCE function
+# Write your MySQL query statement below
+SELECT u.name, COALESE(SUM(r.distance),0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r ON u.id = r.user_id
+GROUP BY r.user_id
+ODER BY travelled_distance DESC, u.name;
